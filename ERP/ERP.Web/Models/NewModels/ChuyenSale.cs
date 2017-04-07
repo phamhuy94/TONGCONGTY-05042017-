34797ff4b3adc595cab46759bebe6b67671f91ec
phamhuy94 <phamquanghuy.94hubt@gmail.com>
2017-04-07 09:05:16 +0700

    huy-commit1-07/04/2017

--- ERP/ERP.Web/Models/NewModels/ChuyenSale.cs
+++ ERP/ERP.Web/Models/NewModels/ChuyenSale.cs
@@ -20,5 +20,8 @@ namespace ERP.Web.Models.NewModels
         public string SALE_CU_2 { set; get; }   
         public string HO_VA_TEN { set; get; }
         public string TEN_SALE_HIEN_THOI { set; get; }
+        public string TEN_SALE_SAP_CHUYEN { set; get; }
+        public string TEN_SALE_CU { set; get; }
+        public string TEN_SALE_CU_HON { set; get; }
     }
 }
