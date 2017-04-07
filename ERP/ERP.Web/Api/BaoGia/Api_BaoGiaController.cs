e06751ba0de6657e2fff98a1203cdbde1f376eff
phamhuy94 <phamquanghuy.94hubt@gmail.com>
2017-04-07 21:40:00 +0700

    huy-cm-07042017

--- ERP/ERP.Web/Api/BaoGia/Api_BaoGiaController.cs
+++ ERP/ERP.Web/Api/BaoGia/Api_BaoGiaController.cs
@@ -9,17 +9,27 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using ERP.Web.Models.Database;
+using System.Data.SqlClient;
+using ERP.Web.Models.BusinessModel;
 
 namespace ERP.Web.Api.BaoGia
 {
     public class Api_BaoGiaController : ApiController
     {
         private ERP_DATABASEEntities db = new ERP_DATABASEEntities();
-
+        XuLyNgayThang xlnt = new XuLyNgayThang();
         // GET: api/Api_BaoGia
         public IQueryable<BH_BAO_GIA> GetBH_BAO_GIA()
         {
             return db.BH_BAO_GIA;
+        }
+
+        [Route("api/Api_BaoGia/GetLienHeKhach/{makhachhang}")]
+        public List<GetAll_LienHeTheoKhach_Result> GetLienHeKhach(string makhachhang)
+        {
+            var query = db.Database.SqlQuery<GetAll_LienHeTheoKhach_Result>("GetAll_LienHeTheoKhach @makhachhang", new SqlParameter("makhachhang", makhachhang));
+            var result = query.ToList();
+            return result;
         }
 
         // GET: api/Api_BaoGia/5
@@ -74,12 +84,63 @@ namespace ERP.Web.Api.BaoGia
         [ResponseType(typeof(BH_BAO_GIA))]
         public IHttpActionResult PostBH_BAO_GIA(BH_BAO_GIA bH_BAO_GIA)
         {
+            string sobaogia;
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+            String nam = DateTime.Today.Year.ToString();
+            String nam2so = nam.Substring(2);
+            var query = db.Database.SqlQuery<string>("XL_LayBaoGiaMoiNhat");
 
-            db.BH_BAO_GIA.Add(bH_BAO_GIA);
+
+            if (query.Count() > 0)
+            {
+                string prefixID = "BG" + nam2so;
+                var data = query.FirstOrDefault();
+                string LastID = data;
+
+                int nextID = int.Parse(LastID.Remove(0, prefixID.Length)) + 1;
+                int lengthNumerID = LastID.Length - prefixID.Length;
+                string zeroNumber = "";
+                for (int i = 1; i <= lengthNumerID; i++)
+                {
+                    if (nextID < Math.Pow(10, i))
+                    {
+                        for (int j = 1; j <= lengthNumerID - i; i++)
+                        {
+                            zeroNumber += "0";
+                        }
+                    }
+                }
+                // int ma = Convert.ToInt32(makhach.Substring(4));
+                sobaogia = prefixID + zeroNumber + nextID.ToString();
+            }
+            else
+                sobaogia = "BG" + nam2so + "0001";
+
+
+            BH_BAO_GIA baogia = new BH_BAO_GIA();
+            baogia.SO_BAO_GIA = sobaogia;
+            baogia.MA_KHACH_HANG = bH_BAO_GIA.MA_KHACH_HANG;
+            baogia.MA_DU_KIEN = bH_BAO_GIA.MA_DU_KIEN;
+            baogia.NGAY_BAO_GIA = xlnt.Xulydatetime(bH_BAO_GIA.NGAY_BAO_GIA.ToString("dd/MM/yyyy"));
+            baogia.LIEN_HE_KHACH_HANG = bH_BAO_GIA.LIEN_HE_KHACH_HANG;
+            baogia.PHUONG_THUC_THANH_TOAN = bH_BAO_GIA.PHUONG_THUC_THANH_TOAN;
+            baogia.HAN_THANH_TOAN = bH_BAO_GIA.HAN_THANH_TOAN;
+            baogia.HIEU_LUC_BAO_GIA = bH_BAO_GIA.HIEU_LUC_BAO_GIA;
+            baogia.DIEU_KHOAN_THANH_TOAN = bH_BAO_GIA.DIEU_KHOAN_THANH_TOAN;
+            baogia.PHI_VAN_CHUYEN = bH_BAO_GIA.PHI_VAN_CHUYEN;
+            baogia.TONG_TIEN = bH_BAO_GIA.TONG_TIEN;
+            baogia.DA_DUYET = bH_BAO_GIA.DA_DUYET;
+            baogia.DIEU_KHOAN_THANH_TOAN = bH_BAO_GIA.DIEU_KHOAN_THANH_TOAN;
+            baogia.NGUOI_DUYET = bH_BAO_GIA.NGUOI_DUYET;
+            baogia.DA_TRUNG = bH_BAO_GIA.DA_TRUNG;
+            baogia.DA_HUY = bH_BAO_GIA.DA_HUY;
+            baogia.LY_DO_HUY = bH_BAO_GIA.LY_DO_HUY;
+            baogia.SALES_BAO_GIA = bH_BAO_GIA.SALES_BAO_GIA;
+            baogia.TRUC_THUOC = bH_BAO_GIA.TRUC_THUOC;
+            db.BH_BAO_GIA.Add(baogia);
 
             try
             {
@@ -97,7 +158,7 @@ namespace ERP.Web.Api.BaoGia
                 }
             }
 
-            return CreatedAtRoute("DefaultApi", new { id = bH_BAO_GIA.SO_BAO_GIA }, bH_BAO_GIA);
+            return Ok(baogia);
         }
 
         // DELETE: api/Api_BaoGia/5
