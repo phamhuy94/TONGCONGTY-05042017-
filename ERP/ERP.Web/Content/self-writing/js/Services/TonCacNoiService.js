6d53a3c5012997cd5bb4ab406f1b1281caa7498b
lamhien195 <lamhien2901@gmail.com>
2017-04-07 13:37:59 +0700

    HIEN-CM1-07042017

---
+++ ERP/ERP.Web/Content/self-writing/js/Services/TonCacNoiService.js
@@ -0,0 +1,7 @@
+﻿app.service('TonCacNoiService', function ($http) {
+    this.get_dataton = function (machuan) {
+        return $http.get("/api/Api_TonKhoHL/GetHH_TON_KHO/" + machuan).then(function (response) {
+            return response.data;
+        });
+    }
+});
